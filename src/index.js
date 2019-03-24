85619f22e82a69631c95eb80e3d288fba7fff85f
realdennis <realdennis777@gmail.com>
2019-03-24 22:50:26 +0800

    Feat: support exception & onError override.  Fix: call props.onLoad if exists

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
+/**
+ * Flow Description
+ * 
+ * onLoad
+ * => call prop.onLoad (if callable)
+ * => setLoaded 
+ * => render img
+ * 
+ * onError
+ * => setIsError
+ * => Throw Exception
+ * 
+ */
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
-const ImgSuspense = ({ fallback, src, alt, style, ...restProps }) => {
+
+/**
+ * Allow user to pass onLoad handler
+ * Allow user to override onError handler
+ */
+const ImgSuspense = ({ fallback, src, alt, style, onLoad, ...restProps }) => {
   const [isLoaded, setLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
+  if (isError) throw new Error('img onerror');
   return (
     <React.Fragment>
       <img
         src={src}
         alt={alt}
+        style={isLoaded ? style : { display: 'none' }}
+        onLoad={() => {
+          typeof onLoad === 'function' && onLoad();
+          setLoaded(true);
+        }}
+        onError={() => setIsError(true)}
         {...restProps}
-        style={isLoaded ? style : Object.assign({}, style, { display: 'none' })}
-        onLoad={() => setLoaded(true)}
       />
       {!isLoaded && fallback}
     </React.Fragment>
