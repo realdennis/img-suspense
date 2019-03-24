89771ad2ab8679eb91af65f04d392d9670c91179
realdennis <realdennis777@gmail.com>
2019-03-25 00:01:47 +0800

    Make code readable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /**
  * Flow Description
- * 
+ *
  * onLoad
  * => call prop.onLoad (if callable)
- * => setLoaded 
+ * => setLoaded
  * => render img
- * 
+ *
  * onError
  * => setIsError
  * => Throw Exception
- * 
+ *
  */
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
@@ -18,7 +18,15 @@ import Proptypes from 'prop-types';
  * Allow user to pass onLoad handler
  * Allow user to override onError handler
  */
-const ImgSuspense = ({ fallback, src, alt, style, onLoad, ...restProps }) => {
+const ImgSuspense = ({
+  fallback,
+  src,
+  alt,
+  style,
+  onLoad,
+  onError,
+  ...restProps
+}) => {
   const [isLoaded, setLoaded] = useState(false);
   const [isError, setIsError] = useState(false);
   if (isError) throw new Error('img onerror');
@@ -30,9 +38,14 @@ const ImgSuspense = ({ fallback, src, alt, style, onLoad, ...restProps }) => {
         style={isLoaded ? style : { display: 'none' }}
         onLoad={() => {
           typeof onLoad === 'function' && onLoad();
+          // If onLoad exists, call it.
+
           setLoaded(true);
         }}
-        onError={() => setIsError(true)}
+        onError={
+          () => (typeof onError === 'function' ? onError() : setIsError(true))
+          // If onError exists, call it and should not throw exception.
+        }
         {...restProps}
       />
       {!isLoaded && fallback}
@@ -41,6 +54,8 @@ const ImgSuspense = ({ fallback, src, alt, style, onLoad, ...restProps }) => {
 };
 ImgSuspense.propTypes = {
   src: Proptypes.string.isRequired,
-  fallback: Proptypes.element.isRequired
+  fallback: Proptypes.element.isRequired,
+  onLoad: Proptypes.func,
+  onError: Proptypes.func
 };
 export default ImgSuspense;
